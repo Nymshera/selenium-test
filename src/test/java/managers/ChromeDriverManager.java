202343cd22e9268001fc18cd6e830b1290a4137f
Jessica Miranda Pérez <jess@MacBook-Pro-de-Jessica.local>
2024-01-12 01:01:14 +0100

    add new files

---
+++ src/test/java/managers/ChromeDriverManager.java
@@ -0,0 +1,14 @@
+package managers;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriverManager extends DriverManager{
+
+    @Override
+    public void createDriver() {
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+
+}
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,50 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class BasePage {
+
+    private static WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        driver = this.driver; 
+    }
+
+    public void clickElement(WebElement element) throws Exception {
+        try {
+             element.click();
+        } catch (Exception e) {
+            throw new Exception("The element " + element + " can't be clicked"); 
+        }
+       
+    }
+
+    public boolean isDisplayed(WebElement element) throws Exception {
+        try {
+            return element.isDisplayed();
+        }catch (Exception e) {
+            throw new Exception("The element " + element + " isn't displayed");
+        }
+
+    }
+
+    public String getText(WebElement element) throws Exception {
+        try {
+            return element.getText();
+        }catch (Exception e) {
+            throw new Exception("The text of the element " + element + " can't be obtained");
+        }
+
+    }
+
+    public String getTitle() throws Exception {
+        try {
+            return driver.getTitle();
+        }catch (Exception e) {
+            throw new Exception("The title can't be obtained");
+        }
+
+    }
     
 }
