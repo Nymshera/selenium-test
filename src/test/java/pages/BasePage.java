3da9bbb5dc1edfc0e8f48452331d6587cb3563df
Jessica Miranda Pérez <jess@MacBook-Pro-de-Jessica.local>
2024-02-26 20:00:00 +0100

    add news methods

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 import utils.TestContext;
 
 public class BasePage {
@@ -33,7 +37,7 @@ public class BasePage {
         }
     }
 
-    public boolean isDisplayed(String locator) throws Exception {
+    public boolean elementIsDisplayed(String locator) throws Exception {
         try {
             return find(locator).isDisplayed();
         } catch (Exception e) {
@@ -67,12 +71,44 @@ public class BasePage {
     }
         
 
-    public void write(String locator, String textToWrite) throws Exception {
+    public void write(String locator, String keysToSend) throws Exception {
         try {
             find(locator).clear();
-            find(locator).sendKeys(textToWrite);
+            find(locator).sendKeys(keysToSend);
         } catch (Exception e) {
-            throw new Exception("The text couldn't be written");
+            throw new Exception("The text " + keysToSend + " couldn't be written");
         }
     }
+
+    public void selectFromDropdownByValue(String locator, String valueToSelect) throws Exception {
+        Select dropdown = new Select(find(locator));
+        dropdown.selectByValue(valueToSelect);
+    }
+
+    public void selectFromDropdownByIndex(String locator, int valueToSelect) throws Exception {
+        Select dropdown = new Select(find(locator));
+        dropdown.selectByIndex(valueToSelect);
+    }
+
+    public void selectFromDropdownByText(String locator, String valueToSelect) throws Exception {
+        Select dropdown = new Select(find(locator));
+        dropdown.selectByVisibleText(valueToSelect);
+    }
+
+    public int dropdownSize(String locator) throws Exception{
+        Select dropdown = new Select(find(locator));
+        List<WebElement> dropdownOptions = dropdown.getOptions();
+        return dropdownOptions.size();
+    }
+
+    public List<String> getDropdownValues(String locator) throws Exception {
+        Select dropdown = new Select(find(locator));
+        List<WebElement> dropdownOptions = dropdown.getOptions();
+        List<String> values = new ArrayList<>();
+        for (WebElement option : dropdownOptions) {
+            values.add(option.getText());
+        }
+        return values;
+    }
+
 }
