1aebd75dcaf27f8b8b948febb3ea6493e7e0d547
Jessica Miranda Pérez <jess@MacBook-Pro-de-Jessica.local>
2024-02-28 11:41:51 +0100

    add new feature

---
+++ src/test/java/pages/FreeRangeTestersPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class FreeRangeTestersPage extends BasePage{
+
+    public FreeRangeTestersPage() {
+        super(driver);
+    }
+    
+}
--- src/test/java/steps/TrademeSteps.java
+++ src/test/java/steps/TrademeSteps.java
@@ -1,7 +1,6 @@
 package steps;
 
 import org.junit.Assert;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
